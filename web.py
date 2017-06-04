df68e475e3aeb5862708a1e2e17ecd3459555016
zhoujun <zjouusky@163.com>
2017-06-04 10:20:51 +0800

    修改主入口名

---
+++ web.py
@@ -0,0 +1,57 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
+from models import Shorturl,short_url_method
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('base.html')
+
+
+@app.route('/index')
+def get_index():
+    return render_template('index.html')
+
+
+@app.route('/back',methods=['get'])
+def get_back():
+    return render_template('back.html')
+
+
+@app.route('/short',methods=['POST'])
+def url_short():
+    url = request.form.get('ori_url','')
+    short_url = Shorturl(url)
+    short_url.save()
+    s_url = request.host+'/sh/'+short_url.short_url
+    return render_template('index.html',value=s_url)
+
+
+@app.route('/sh/<surl>/',methods=['get'])
+def url_trans(surl):
+    url = Shorturl.find_by(short_url=surl)
+    if url:
+        return redirect(url.ori_url,302)
+    else:
+        return redirect(url_for("/index"))
+
+
+@app.route('/back',methods=['post'])
+def url_back():
+    short_url = request.form.get('short_url', '')
+    new = short_url.split('/')[-1]
+    url = Shorturl.find_by(short_url=new)
+    if url:
+        return render_template('back.html',value=url.ori_url)
+    else:
+        return redirect('/back')
+
+
+if __name__ == '__main__':
+    app.run()
